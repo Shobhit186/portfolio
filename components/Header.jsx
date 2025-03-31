c425907bbf8c7a2ef9404f0024c1db25151ceb6c
Shobhit Dwivedi <Shobhit.Dwivedi@gds.ey.com>
2025-03-31 16:38:21 +0530

    updated the logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Button } from './ui/button';
 import Navbar from './Navbar';
 import MobileNavbar from './MobileNavbar';
@@ -9,7 +10,7 @@ const Header = () => {
     <header className='py-8 xl:py-12 text-white bg-pink-50/20'>
       <div className="container mx-auto flex justify-between items-center px-4 sm:px-8 lg:px-16 xl:px-24">
         <Link href="/">
-          <h1 className='text-4xl font-semibold'>Shobhit<span className='text-[var(--color-accent)] hover:text-[var(--color-accent-foreground)]'>.</span></h1>
+          <h1 className='text-4xl font-semibold'><Image src="/portfolio-icon-vector-10.jpg" alt="logo" width={60} height={60}/></h1>
         </Link>
 
         <div className="hidden xl:flex items-center gap-8">
